282e6006c3f34ffbcc7817ee40e2e392425c7e2d
chaykinks <chaykinks@gmail.com>
2026-03-08 18:15:32 +0500

    добавил компонент private-route

---
+++ src/components/private-route/private-route.tsx
@@ -0,0 +1,17 @@
+import { Navigate } from 'react-router-dom';
+import { AuthorizationStatus } from '../../const';
+
+type PrivateRouteProps = {
+  authorizationStatus: AuthorizationStatus;
+  children: JSX.Element;
+};
+
+function PrivateRoute({authorizationStatus, children,}: PrivateRouteProps): JSX.Element {
+  return authorizationStatus === AuthorizationStatus.Auth ? (
+    children
+  ) : (
+    <Navigate to="/login" />
+  );
+}
+
+export default PrivateRoute;

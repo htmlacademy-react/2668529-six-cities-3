116d2a9b7d4ebdb7b6fc66ddb1141a73c5a4ce08
chaykinks <chaykinks@gmail.com>
2026-03-08 11:24:57 +0500

    вынес массив офферов из мэйн пейдж

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,61 +1,9 @@
 import PlaceCard from '../../components/place-card/place-card.tsx';
+import { offers } from "../../const.ts";
 
 type MainPageProps = {
   offersCount: number;
 };
-
-const offers = [
-  {
-    id: 1,
-    isPremium: true,
-    imageSrc: 'img/apartment-01.jpg',
-    price: 120,
-    isBookmarked: false,
-    ratingPercent: 80,
-    title: 'Beautiful & luxurious apartment at great location',
-    offerType: 'Apartment',
-  },
-  {
-    id: 2,
-    isPremium: false,
-    imageSrc: 'img/room.jpg',
-    price: 80,
-    isBookmarked: true,
-    ratingPercent: 80,
-    title: 'Wood and stone place',
-    offerType: 'Room',
-  },
-  {
-    id: 3,
-    isPremium: false,
-    imageSrc: 'img/apartment-02.jpg',
-    price: 132,
-    isBookmarked: false,
-    ratingPercent: 80,
-    title: 'Canal View Prinsengracht',
-    offerType: 'Apartment',
-  },
-  {
-    id: 4,
-    isPremium: true,
-    imageSrc: 'img/apartment-03.jpg',
-    price: 180,
-    isBookmarked: false,
-    ratingPercent: 100,
-    title: 'Nice, cozy, warm big bed apartment',
-    offerType: 'Apartment',
-  },
-  {
-    id: 5,
-    isPremium: false,
-    imageSrc: 'img/room.jpg',
-    price: 80,
-    isBookmarked: true,
-    ratingPercent: 80,
-    title: 'Wood and stone place',
-    offerType: 'Room',
-  },
-];
 
 function MainPage({offersCount}: MainPageProps): JSX.Element {
   return (

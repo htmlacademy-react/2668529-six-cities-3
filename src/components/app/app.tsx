c8ae4a39a22b84c737913c6d5457b890a98cf56c
chaykinks <chaykinks@gmail.com>
2026-03-08 18:26:46 +0500

    добавил маршрутизацию по страницам в app.tsx

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,10 @@
+import { Route, Routes } from 'react-router-dom';
 import MainPage from '../../pages/main-page/main-page.tsx';
+import LoginPage from '../../pages/login-page/login-page';
+import FavoritesPage from '../../pages/favourites-page/favourites-page';
+import OfferPage from '../../pages/offer-page/offer-page';
+import NotFoundPage from '../../pages/not-found-page/not-found-page';
+import PrivateRoute from '../private-route/private-route';
 import {AuthorizationStatus} from '../../const';
 
 type AppProps = {
@@ -9,10 +15,37 @@ type AppProps = {
 function App({offersCount, authorizationStatus}: AppProps): JSX.Element {
 
   return (
-    <MainPage
-      offersCount={offersCount}
-      authorizationStatus={authorizationStatus}
-    />
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <MainPage
+            offersCount={offersCount}
+            authorizationStatus={authorizationStatus}
+          />
+        }
+      />
+      <Route
+        path="/login"
+        element={<LoginPage />}
+      />
+      <Route
+        path="/favorites"
+        element={
+          <PrivateRoute authorizationStatus={authorizationStatus}>
+            <FavoritesPage />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/offer/:id"
+        element={<OfferPage />}
+      />
+      <Route
+        path="*"
+        element={<NotFoundPage />}
+      />
+    </Routes>
   );
 }
 

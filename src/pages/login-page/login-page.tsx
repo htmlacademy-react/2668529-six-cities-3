92dcc5ca8f27e38e6e1761dcd1c250095af976b5
chaykinks <chaykinks@gmail.com>
2026-03-10 20:21:22 +0500

    добавил пропущенный блок в логин пейдж

--- src/pages/login-page/login-page.tsx
+++ src/pages/login-page/login-page.tsx
@@ -32,6 +32,14 @@ function LoginPage(): JSX.Element {
               </button>
             </form>
           </section>
+
+          <section className="locations locations--login locations--current">
+            <div className="locations__item">
+              <a className="locations__item-link" href="#todo">
+                <span>Amsterdam</span>
+              </a>
+            </div>
+          </section>
         </div>
       </main>
     </div>

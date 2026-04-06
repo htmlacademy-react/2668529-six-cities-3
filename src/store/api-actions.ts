751b4f5b0f0ae8d3574da5e4cb54e44879a5089d
chaykinks <chaykinks@gmail.com>
2026-04-06 20:33:00 +0500

    добавил запросы получения nearbyoffers, комментариев и отправки комментария в api-actions

--- src/store/api-actions.ts
+++ src/store/api-actions.ts
@@ -1,8 +1,10 @@
 import {AxiosInstance} from 'axios';
 import {AppDispatch, State} from './index';
-import {fillOffers, setOffersLoadingStatus, fillCurrentOffer,
-  setCurrentOfferLoadingStatus, requireAuthorization} from './action';
+import {fillOffers, setOffersLoadingStatus, fillCurrentOffer, setCurrentOfferLoadingStatus,
+  fillNearbyOffers, setNearbyOffersLoadingStatus, fillReviews, setReviewsLoadingStatus, setReviewSendingStatus,
+  requireAuthorization} from './action';
 import {Offer, FullOffer} from '../types/offer';
+import {Review} from '../types/review';
 import {AuthorizationStatus} from '../const';
 import {saveToken, dropToken} from '../services/token.ts';
 
@@ -25,6 +27,12 @@ type AuthInfo = {
   name: string;
 };
 
+type ReviewData = {
+  offerId: string;
+  comment: string;
+  rating: number;
+};
+
 export const checkAuthAction = (): ThunkActionResult =>
   async (dispatch, _getState, api) => {
     try {
@@ -38,7 +46,6 @@ export const checkAuthAction = (): ThunkActionResult =>
 export const loginAction = ({ email, password }: AuthData): ThunkActionResult =>
   async (dispatch, _getState, api) => {
     const { data } = await api.post<AuthInfo>('/login', { email, password });
-
     saveToken(data.token);
     dispatch(requireAuthorization(AuthorizationStatus.Auth));
   };
@@ -72,5 +79,43 @@ export const fetchCurrentOfferAction = (id: string): ThunkActionResult =>
       dispatch(fillCurrentOffer(data));
     } finally {
       dispatch(setCurrentOfferLoadingStatus(false));
+    }
+  };
+
+export const fetchNearbyOffersAction = (id: string): ThunkActionResult =>
+  async (dispatch, _getState, api) => {
+    dispatch(setNearbyOffersLoadingStatus(true));
+    dispatch(fillNearbyOffers([]));
+
+    try {
+      const { data } = await api.get<Offer[]>(`/offers/${id}/nearby`);
+      dispatch(fillNearbyOffers(data));
+    } finally {
+      dispatch(setNearbyOffersLoadingStatus(false));
+    }
+  };
+
+export const fetchReviewsAction = (id: string): ThunkActionResult =>
+  async (dispatch, _getState, api) => {
+    dispatch(setReviewsLoadingStatus(true));
+    dispatch(fillReviews([]));
+
+    try {
+      const { data } = await api.get<Review[]>(`/comments/${id}`);
+      dispatch(fillReviews(data));
+    } finally {
+      dispatch(setReviewsLoadingStatus(false));
+    }
+  };
+
+export const sendReviewAction = ({offerId, comment, rating}: ReviewData): ThunkActionResult =>
+  async (dispatch, _getState, api) => {
+    dispatch(setReviewSendingStatus(true));
+
+    try {
+      const { data } = await api.post<Review[]>(`/comments/${offerId}`, {comment, rating});
+      dispatch(fillReviews(data));
+    } finally {
+      dispatch(setReviewSendingStatus(false));
     }
   };
